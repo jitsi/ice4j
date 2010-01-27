579c13c4a03d71d7802aae9eaec62e37129d143f
emcho@sip-communicator.org <emcho@sip-communicator.org@424aafce-ff79-11de-b136-7f7962f7bc17>
2010-01-27 18:17:56 +0000

    Ongoing work on a draft ICE implementation

--- test/test/Ice.java
+++ test/test/Ice.java
@@ -7,6 +7,10 @@
  */
 package test;
 
+import java.lang.reflect.*;
+import java.net.*;
+import java.util.*;
+
 import org.ice4j.*;
 import org.ice4j.ice.*;
 
@@ -17,20 +21,129 @@ import org.ice4j.ice.*;
  */
 public class Ice
 {
-
     /**
      * Runs the test
      * @param args cmd line args
+     *
+     * @throws Throwable if bad stuff happens.
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws Throwable
     {
         Agent agent = new Agent();
         IceMediaStream stream = agent.createMediaStream("audio");
 
         //rtp
+        Component rtpComp = agent.createComponent(
+                        stream, Transport.UDP, 9090, 9090, 10000);
+        System.out.println("rtpComp:" + rtpComp);
+        //rtcpComp
+        Component rtcpComp = agent.createComponent(
+                        stream, Transport.UDP, 9091, 9090, 10000);
+
+        System.out.println("rtcpComp:" + rtcpComp);
+    }
+
+    /**
+     * Runs the test
+     * @param args cmd line args
+     *
+     * @throws Throwable if bad stuff happens.
+     */
+    public static void main2(String[] args) throws Throwable
+    {
+        //Agent agent = new Agent();
+        //IceMediaStream stream = agent.createMediaStream("audio");
+
+        //rtp
         //Component rtpComp = stream.createComponent(Transport.UDP, 9090);
         //rtcpComp
         //Component rtcpComp = stream.createComponent(Transport.UDP, 9091);
+
+        // find a loopback interface
+
+
+        Enumeration<NetworkInterface> interfaces
+                                    = NetworkInterface.getNetworkInterfaces();
+
+        while (interfaces.hasMoreElements())
+        {
+            NetworkInterface iface = interfaces.nextElement();
+
+            System.out.println(iface.getName()
+                            + " isLoopback=" + isLoop(iface));
+            System.out.println(iface.getName()
+                            + " isUp=" + isUp(iface));
+            System.out.println(iface.getName()
+                            + " isVirtual=" + isVirtual(iface));
+        }
     }
 
+    public static boolean isLoop(NetworkInterface iface)
+    {
+        try
+        {
+            Method method
+                = iface.getClass().getMethod("isLoopback", new Class[]{});
+
+            System.out.println("It works!");
+
+            return ((Boolean)method.invoke(iface, new Object[]{}))
+                        .booleanValue();
+        }
+        catch(Throwable t)
+        {
+            //apparently we are not running in a JVM that supports the
+            //is Loopback method. we'll try another approach.
+            System.out.println("Doesn't work");
+        }
+
+        Enumeration<InetAddress> addresses = iface.getInetAddresses();
+
+        return addresses.hasMoreElements()
+            && addresses.nextElement().isLoopbackAddress();
+    }
+
+    public static boolean isUp(NetworkInterface iface)
+    {
+        try
+        {
+            Method method
+                = iface.getClass().getMethod("isUp", new Class[]{});
+
+            System.out.println("It works!");
+
+            return ((Boolean)method.invoke(iface, new Object[]{}))
+                        .booleanValue();
+        }
+        catch(Throwable t)
+        {
+            //apparently we are not running in a JVM that supports the
+            //is Loopback method. we'll try another approach.
+            System.out.println("Doesn't work");
+        }
+
+        return true;
+    }
+
+    public static boolean isVirtual(NetworkInterface iface)
+    {
+        try
+        {
+            Method method
+                = iface.getClass().getMethod("isVirtual", new Class[]{});
+
+            System.out.println("It works!");
+
+            return ((Boolean)method.invoke(iface, new Object[]{}))
+                        .booleanValue();
+        }
+        catch(Throwable t)
+        {
+            //apparently we are not running in a JVM that supports the
+            //is Loopback method. we'll try another approach.
+            System.out.println("Doesn't work");
+        }
+
+        return false;
+    }
 }
