050db54e757cc761d698e46657fcbc7f8549f865
emcho@sip-communicator.org <emcho@sip-communicator.org@424aafce-ff79-11de-b136-7f7962f7bc17>
2010-04-25 14:39:41 +0000

    Start work on the connectivity checks client and server

---
+++ trunk/src/org/ice4j/ice/ConnectivityCheckServer.java
@@ -0,0 +1,15 @@
+/*
+ * ice4j, the OpenSource Java Solution for NAT and Firewall Traversal.
+ * Maintained by the SIP Communicator community (http://sip-communicator.org).
+ *
+ * Distributable under LGPL license. See terms of license at gnu.org.
+ */
+package org.ice4j.ice;
+
+/**
+ * @author Emil Ivov
+ */
+public class ConnectivityCheckServer
+{
+
+}
