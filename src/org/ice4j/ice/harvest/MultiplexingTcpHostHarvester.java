471f91b364c5fa996f12973554b2fd72e61b445c
boris <boris@sip-communicator.org>
2015-06-02 12:22:14 +0000

    Automatically adds a mapping for TCP when running on EC2.

--- src/org/ice4j/ice/harvest/MultiplexingTcpHostHarvester.java
+++ src/org/ice4j/ice/harvest/MultiplexingTcpHostHarvester.java
@@ -628,6 +628,17 @@ public class MultiplexingTcpHostHarvester
             serverSocketChannels.add(channel);
         }
 
+        // Automatically add a mapping if we're on EC2
+        TransportAddress ec2Face = AwsCandidateHarvester.getFace();
+        TransportAddress ec2Mask;
+        if (ec2Face != null &&
+                (ec2Mask = AwsCandidateHarvester.getMask()) != null)
+        {
+            addMappedAddress(ec2Mask.getAddress(), ec2Face.getAddress());
+            logger.info("Adding an EC2 mapping for TCP: "
+                                + ec2Face + "->" + ec2Mask);
+        }
+
         acceptThread = new AcceptThread();
         acceptThread.start();
 
