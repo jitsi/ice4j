085ae40fd7daf90583f6d982fd4e18731766cffc
Boris Grozev <boris@jitsi.org>
2022-03-03 16:56:19 -0600

    test: Disable config caching for tests.

---
+++ src/test/java/org/ice4j/TestExecutionListener.java
@@ -0,0 +1,47 @@
+/*
+ * ice4j, the OpenSource Java Solution for NAT and Firewall Traversal.
+ *
+ * Copyright @ 2015 Atlassian Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ice4j;
+
+import org.jitsi.metaconfig.*;
+import org.junit.platform.launcher.*;
+
+public class TestExecutionListener implements org.junit.platform.launcher.TestExecutionListener {
+    private static boolean initialized = false;
+
+    private static void beforeAll()
+    {
+        // Disable config caching.
+        MetaconfigSettings.Companion.setCacheEnabled(false);
+    }
+
+    public TestExecutionListener()
+    {}
+
+    @Override
+    public void executionStarted(TestIdentifier testIdentifier)
+    {
+        synchronized (TestExecutionListener.class)
+        {
+            if (!initialized)
+            {
+                beforeAll();
+                initialized = true;
+            }
+        }
+    }
+}
