933885a273a6020758ebb5c921a68afd727a9fb7
Jonathan Lennox <jonathan.lennox@8x8.com>
2024-10-14 17:14:26 -0400

    Add socket pool, to allow sending to a single socket from multiple threads without blocking.

---
+++ src/main/kotlin/org/ice4j/socket/SocketPool.kt
@@ -0,0 +1,133 @@
+/*
+ * Copyright @ 2020 - Present, 8x8 Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ice4j.socket
+
+import java.io.Closeable
+import java.net.DatagramSocket
+import java.net.DatagramSocketImpl
+import java.net.SocketAddress
+import java.nio.channels.DatagramChannel
+import java.util.concurrent.Semaphore
+
+/** A pool of datagram sockets all bound on the same port.
+ *
+ * This is necessary to allow multiple threads to send packets simultaneously from the same source address,
+ * in JDK 15 and later, because the [DatagramChannel]-based implementation of [DatagramSocketImpl] introduced
+ * in that version locks the socket during a call to [DatagramSocket.send].
+ *
+ * (The old [DatagramSocketImpl] implementation can be used by setting the system property
+ * `jdk.net.usePlainDatagramSocketImpl` in JDK versions 15 through 17, but was removed in versions 18 and later.)
+ *
+ * This feature may also be useful on older JDK versions on non-Linux operating systems, such as macOS,
+ * which block simultaneous writes through the same UDP socket at the operating system level.
+ *
+ * The sockets are opened such that packets will be _received_ on exactly one socket.
+ */
+class SocketPool(
+    /** The address to which to bind the pool of sockets. */
+    address: SocketAddress,
+    /** The number of sockets to create for the pool.  If this is set to zero (the default), the number
+     * will be set automatically to an appropriate value.
+     */
+    requestedNumSockets: Int = 0
+) {
+    init {
+        require(requestedNumSockets >= 0) { "RequestedNumSockets must be >= 0" }
+    }
+
+    val numSockets: Int =
+        if (requestedNumSockets != 0) {
+            requestedNumSockets
+        } else {
+            // TODO: set this to 1 in situations where pools aren't needed?
+            Runtime.getRuntime().availableProcessors()
+        }
+
+    private val semaphore = Semaphore(numSockets)
+
+    private val sockets = buildList {
+        val multipleSockets = numSockets > 1
+        var bindAddr = address
+        for (i in 0 until numSockets) {
+            val sock = DatagramSocket(null)
+            if (multipleSockets) {
+                sock.reuseAddress = true
+            }
+            sock.bind(bindAddr)
+            if (i == 0 && multipleSockets) {
+                bindAddr = sock.localSocketAddress
+            }
+            add(sock)
+        }
+    }
+
+    private val availableSockets = ArrayDeque(sockets)
+
+    /** The socket on which packets will be received. */
+    val receiveSocket: DatagramSocket
+        // On all platforms I've tested, the last-bound socket is the one which receives packets.
+        // TODO: should we support Linux's flavor of SO_REUSEPORT, in which packets can be received on *all* the
+        //  sockets, spreading load?
+        get() = sockets.last()
+
+    interface SocketHolder : Closeable {
+        val socket: DatagramSocket
+    }
+
+    /** Socket holder with autocloseable semantics, to ensure the socket is returned. */
+    private inner class MySocketHolder : SocketHolder {
+        override val socket = acquireSendSocket()
+        private var closed = false
+        override fun close() {
+            if (!closed) {
+                returnSendSocket(socket)
+                closed = true
+            }
+        }
+    }
+
+    /** Trivial socket holder that gives out a single unique socket. */
+    private inner class TrivialSocketHolder : SocketHolder {
+        override val socket = sockets.first()
+        override fun close() {}
+    }
+
+    /** Gets a send socket holder.  Should be used with Kotlin [use] or Java try-with-resources.  May block. */
+    fun getSendSocket(): SocketHolder {
+        if (numSockets == 1) {
+            return TrivialSocketHolder()
+        }
+        return MySocketHolder()
+    }
+
+    private fun acquireSendSocket(): DatagramSocket {
+        semaphore.acquire()
+        synchronized(availableSockets) {
+            return availableSockets.removeFirst()
+        }
+    }
+
+    private fun returnSendSocket(socket: DatagramSocket) {
+        semaphore.release()
+        synchronized(availableSockets) {
+            availableSockets.addLast(socket)
+        }
+    }
+
+    fun close() {
+        sockets.forEach { it.close() }
+    }
+}
---
+++ src/test/kotlin/org/ice4j/socket/SocketPoolTest.kt
@@ -0,0 +1,210 @@
+package org.ice4j.socket
+
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.core.test.Enabled
+import io.kotest.core.test.TestCase
+import io.kotest.matchers.comparables.shouldBeLessThan
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.types.beInstanceOf
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetSocketAddress
+import java.net.SocketAddress
+import java.time.Clock
+import java.time.Duration
+import java.time.Instant
+
+class SocketPoolTest : ShouldSpec() {
+    private val loopbackAny = InetSocketAddress("127.0.0.1", 0)
+    private val loopbackDiscard = InetSocketAddress("127.0.0.1", 9)
+
+    init {
+        context("Creating a new socket pool") {
+            val pool = SocketPool(loopbackAny)
+            should("Bind to a random port") {
+                val local = pool.receiveSocket.localSocketAddress
+                local should beInstanceOf<InetSocketAddress>()
+                (local as InetSocketAddress).port shouldNotBe 0
+            }
+            pool.close()
+        }
+
+        context("Getting multiple send sockets from a pool") {
+            val numSockets = 4
+            val pool = SocketPool(loopbackAny, numSockets)
+            val holders = mutableListOf<SocketPool.SocketHolder>()
+            should("be possible") {
+                repeat(numSockets) {
+                    holders.add(pool.getSendSocket())
+                }
+            }
+            holders.forEach { it.close() }
+            pool.close()
+        }
+
+        context("Packets sent from each of the send sockets in the pool") {
+            val numSockets = 4
+            val pool = SocketPool(loopbackAny, numSockets)
+            val local = pool.receiveSocket.localSocketAddress
+            val holders = mutableListOf<SocketPool.SocketHolder>()
+            repeat(numSockets) {
+                holders.add(pool.getSendSocket())
+            }
+            holders.forEachIndexed { i, it ->
+                val buf = i.toString().toByteArray()
+                val packet = DatagramPacket(buf, buf.size, local)
+                it.socket.send(packet)
+            }
+            holders.forEach { it.close() }
+
+            should("be received") {
+                for (i in 0 until numSockets) {
+                    val buf = ByteArray(1500)
+                    val packet = DatagramPacket(buf, buf.size)
+                    pool.receiveSocket.soTimeout = 1 // Don't block if something's wrong
+                    pool.receiveSocket.receive(packet)
+                    packet.data.decodeToString(0, packet.length).toInt() shouldBe i
+                    packet.socketAddress shouldBe local
+                }
+            }
+        }
+
+        context("The number of send sockets") {
+            val numSockets = 4
+            val pool = SocketPool(loopbackAny, numSockets)
+
+            val sockets = mutableSetOf<DatagramSocket>()
+
+            repeat(2 * numSockets) {
+                // This should cycle through all the available send sockets
+                pool.getSendSocket().use { holder ->
+                    sockets.add(holder.socket)
+                }
+            }
+
+            should("be correct") {
+                sockets.size shouldBe numSockets
+            }
+        }
+
+        val disableIfOnlyOneCore: (TestCase) -> Enabled = {
+            if (Runtime.getRuntime().availableProcessors() > 1) {
+                Enabled.enabled
+            } else {
+                Enabled.disabled("Need multiple processors to run test")
+            }
+        }
+
+        context("Sending packets from multiple threads").config(enabledOrReasonIf = disableIfOnlyOneCore) {
+            val poolWarmup = SocketPool(loopbackAny, 1)
+            sendTimeOnAllSockets(poolWarmup)
+
+            val pool1 = SocketPool(loopbackAny, 1)
+            val elapsed1 = sendTimeOnAllSockets(pool1)
+
+            // 0 means pick the default value, currently Runtime.getRuntime().availableProcessors().
+            val poolN = SocketPool(loopbackAny, 0)
+            val elapsedN = sendTimeOnAllSockets(poolN)
+
+            elapsedN shouldBeLessThan elapsed1 // Very weak test
+        }
+
+        context("Test sending packets from multiple threads") {
+            val poolWarmup = SocketPool(loopbackAny, 1)
+            sendTimeOnAllSockets(poolWarmup)
+
+            val pool1 = SocketPool(loopbackAny, 1)
+            val elapsed11 = sendTimeOnAllSockets(pool1)
+            println("Send ${Sender.NUM_PACKETS} packets on 1 socket on one thread took $elapsed11")
+
+            val numProcessors = Runtime.getRuntime().availableProcessors()
+
+            val elapsed1N = sendTimeOnAllSockets(pool1, numProcessors)
+            println(
+                "Send ${Sender.NUM_PACKETS} packets on 1 socket on $numProcessors threads took $elapsed1N"
+            )
+
+            val poolN = SocketPool(loopbackAny, numProcessors)
+            val elapsedNN = sendTimeOnAllSockets(poolN, numProcessors)
+            println(
+                "Send ${Sender.NUM_PACKETS} packets on $numProcessors sockets on $numProcessors threads " +
+                    "took $elapsedNN"
+            )
+
+            val elapsedN2N = sendTimeOnAllSockets(poolN, 2 * numProcessors)
+            println(
+                "Send ${Sender.NUM_PACKETS} packets on $numProcessors sockets on ${2 * numProcessors} threads " +
+                    "took $elapsedN2N"
+            )
+
+            val pool2N = SocketPool(loopbackAny, 2 * numProcessors)
+            val elapsed2N2N = sendTimeOnAllSockets(pool2N, 2 * numProcessors)
+            println(
+                "Send ${Sender.NUM_PACKETS} packets on ${2 * numProcessors} sockets " +
+                    "on ${2 * numProcessors} threads took $elapsed2N2N"
+            )
+        }
+    }
+
+    private class Sender(
+        private val count: Int,
+        private val pool: SocketPool,
+        private val destAddr: SocketAddress
+    ) : Runnable {
+        private val buf = ByteArray(BUFFER_SIZE)
+
+        private fun sendToSocket(count: Int) {
+            pool.getSendSocket().use {
+                for (i in 0 until count) {
+                    it.socket.send(DatagramPacket(buf, BUFFER_SIZE, destAddr))
+                }
+            }
+        }
+
+        override fun run() {
+            val startTime: Instant = clock.instant()
+
+            sendToSocket(count)
+
+            val endTime: Instant = clock.instant()
+
+            val myElapsed: Duration = Duration.between(startTime, endTime)
+            setElapsed(myElapsed)
+        }
+
+        companion object {
+            private const val BUFFER_SIZE = 1500
+            const val NUM_PACKETS = 600000
+            private val clock = Clock.systemUTC()
+
+            var elapsed: Duration = Duration.ZERO
+                private set
+
+            fun setElapsed(myElapsed: Duration) {
+                synchronized(this) {
+                    if (elapsed < myElapsed) {
+                        elapsed = myElapsed
+                    }
+                }
+            }
+
+            fun resetElapsed() {
+                elapsed = Duration.ZERO
+            }
+        }
+    }
+
+    private fun sendTimeOnAllSockets(pool: SocketPool, numThreads: Int = pool.numSockets): Duration {
+        val threads = mutableListOf<Thread>()
+        Sender.resetElapsed()
+        repeat(numThreads) {
+            val thread = Thread(Sender(Sender.NUM_PACKETS / numThreads, pool, loopbackDiscard))
+            threads.add(thread)
+            thread.start()
+        }
+        threads.forEach { it.join() }
+        return Sender.elapsed
+    }
+}
