902aca9c8e356f8298b0867f28043f63c141dfea
Jonathan Lennox <jonathan.lennox@8x8.com>
2024-10-21 17:04:50 -0400

    Change default pool size.

--- src/main/kotlin/org/ice4j/socket/SocketPool.kt
+++ src/main/kotlin/org/ice4j/socket/SocketPool.kt
@@ -57,7 +57,7 @@ class SocketPool(
             requestedNumSockets
         } else {
             // TODO: set this to 1 in situations where pools aren't needed?
-            Runtime.getRuntime().availableProcessors()
+            2 * Runtime.getRuntime().availableProcessors()
         }
 
     private val sockets = buildList {
